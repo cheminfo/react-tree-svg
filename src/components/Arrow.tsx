8f87c99dd4320e2694b2303c58891ac75c2572e2
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-06-27 10:26:03 +0200

    chore: arrow aligned to arrow head start

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,7 +1,7 @@
 import { CenteredText } from "./CenteredText";
 import { Point } from "../types/Point";
 import { RightText } from "./RightText";
-
+import { MarkerDef } from "./MarkerDef";
 /**
  * Creates a simple arrow between 2 points. The arrow has one inflection point, which is the middle of the line.
  * The SVG must define a marker with id="arrowhead" for this to work.
@@ -17,7 +17,16 @@ export function Arrow(props: {
 }) {
   const { from, to, label, labelPosition } = props;
   const middle = { x: (from.x + to.x) / 2, y: (from.y + to.y) / 2 };
-  const d = `M ${from.x} ${from.y} C ${middle.x} ${from.y}  ${middle.x} ${to.y}  ${to.x} ${to.y}`;
+  const refX = MarkerDef(props).props.children.props.refX;
+  console.log(to.x);
+  console.log(to.x - refX);
+  console.log(to.x - refX * 2);
+  const headInflectionPoint = {
+    x: to.x - refX * 2, // refX attribute of the marker element in the Arrow component shifts the arrow head to the left, and that the vertices of the arrow head are set to refX, which means that the beginning of the arrow head is refX * 2 to the left.
+    y: to.y,
+  };
+  const d = `M ${from.x} ${from.y} C ${middle.x} ${from.y}  ${middle.x} ${to.y}  ${headInflectionPoint.x} ${to.y} Q ${headInflectionPoint.x} ${to.y}  ${to.x} ${to.y} `;
+
   //const d = `M ${from.x} ${from.y} C ${middle.x} ${from.y}  ${middle.x} ${from.y}  ${middle.x} ${middle.y} ${middle.x} ${to.y} ${middle.x} ${to.y}  ${to.x} ${to.y}`;
   return (
     <g>
