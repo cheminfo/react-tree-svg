57ef3787eb9ff33ece050ab134aabb2d1e58f383
Luc Patiny <luc@patiny.com>
2023-09-25 09:28:00 +0200

    fix: correct taxonomy renderer export name

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { render } from './render';
 
 export { moleculeRenderer } from './nodeRenderer/moleculeRenderer';
-export { taxonomyRender } from './nodeRenderer/taxonomyRenderer';
+export { taxonomyRenderer } from './nodeRenderer/taxonomyRenderer';
